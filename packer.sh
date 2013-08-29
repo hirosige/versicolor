7fa323e4e1fc12ae56ee170bc02182e772a7c7de
ryurock <ryusukekimura3@gmail.com>
2013-08-29 16:53:29 +0900

    Packerのプロビジョニングでansibleを追加

--- packer.sh
+++ packer.sh
@@ -2,8 +2,8 @@
 #
 set -e
 
-if [ -e ~/.vagrant.d/boxes/virtualbox/CentOS6.4-x86-64-minimal ]; then
-    rm -rf ~/.vagrant.d/boxes/virtualbox/CentOS6.4-x86-64-minimal
+if [ -e ~/.vagrant.d/boxes/CentOS6.4-x86-64-minimal/virtualbox ]; then
+    rm -rf ~/.vagrant.d/boxes/CentOS6.4-x86-64-minimal/virtualbox
     echo "boxes deleted"
 fi
 
