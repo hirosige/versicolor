5abec070ab7c1c6eaf4d24fd17e2415a186ef627
ryurock <ryusukekimura3@gmail.com>
2013-09-11 11:44:32 +0900

    packerに必要なファイルをpackerディレクトリに移動

--- packer/provisioners/ansible.sh
+++ packer/provisioners/ansible.sh
@@ -14,7 +14,8 @@ make install
 # ansible localhost settings
 echo "export ANSIBLE_HOSTS=/vagrant/ansible_hosts" > /etc/profile.d/ansible.sh
 
-curl https://raw.github.com/mitchellh/vagrant/master/keys/vagrant > ~/.ssh/id_rsa; chmod 600 ~/.ssh/id_rsa
-ssh-agent bash
-ssh-add ~/.ssh/id_rsa
+#mkdir -p /home/vagrant/.ssh/
+#curl https://raw.github.com/mitchellh/vagrant/master/keys/vagrant > /home/vagrant/.ssh/id_rsa; chmod 600 /home/vagrant/.ssh/id_rsa
+#ssh-agent bash
+#ssh-add /home/vagrant/.ssh/id_rsa
 
