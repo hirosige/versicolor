ecbbd431bb58bc3628c7536fb35fbf9df0cd451b
ryurock <ryusukekimura3@gmail.com>
2013-09-11 15:46:30 +0900

    秘密鍵も一緒に取得する

--- packer/provisioners/ansible.sh
+++ packer/provisioners/ansible.sh
@@ -13,9 +13,3 @@ make install
 
 # ansible localhost settings
 echo "export ANSIBLE_HOSTS=/vagrant/ansible_hosts" > /etc/profile.d/ansible.sh
-
-#mkdir -p /home/vagrant/.ssh/
-#curl https://raw.github.com/mitchellh/vagrant/master/keys/vagrant > /home/vagrant/.ssh/id_rsa; chmod 600 /home/vagrant/.ssh/id_rsa
-#ssh-agent bash
-#ssh-add /home/vagrant/.ssh/id_rsa
-
--- packer/provisioners/vagrant.sh
+++ packer/provisioners/vagrant.sh
@@ -5,6 +5,8 @@ date > /etc/vagrant_box_build_time
 mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant' -O /home/vagrant/.ssh/id_rsa
+chmod 0600 /home/vagrant/.ssh/id_rsa
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
 # Customize the message of the day
